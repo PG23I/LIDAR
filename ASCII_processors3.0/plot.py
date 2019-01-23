312cba352e9f6da101045c6de0e7a14fc1aebd97
Prannav Gupta <prannavgupta23@outlook.com>
2019-01-23 16:17:09 -0600

    Move to github

---
+++ ASCII_processors3.0/plot.py
@@ -0,0 +1,20 @@
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x, y, z = np.loadtxt('writefile.txt', delimiter=',', unpack=True)
+
+
+
+ax.scatter(x, y, z, c='r', marker='o')
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
