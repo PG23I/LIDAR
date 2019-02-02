4b9c6e2f4c25ca7cdc55c5b958eb6aa552e5b13f
Prannav Gupta <prannavgupta23@outlook.com>
2019-02-02 14:52:10 -0600

    Data Visualization IP

--- ASCII_processors3.0/plot.py
+++ ASCII_processors3.0/plot.py
@@ -4,17 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-x, y, z = np.loadtxt('writefile.txt', delimiter=',', unpack=True)
+"""fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')"""
 
 
+x, y, z, intensity, photon_count = np.loadtxt('writefile.txt', delimiter=',', unpack=True)
 
-ax.scatter(x, y, z, c='r', marker='o')
+X, Y = np.meshgrid(x, y)
+Z = np.reshape(photon_count, (-1, 2))
+#Z = np.array(photon_count)
+#Z = Z.reshape((1, 1)
+#Z = photon_count
+#Z=np.random.rand(len(Y),len(X))
+plt.contour(X, Y, Z, colors='black')
+plt.show()
+
+
+
+"""ax.scatter(x, y, z, c='r', marker='o')
 
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 
-plt.show()
+plt.show()"""
