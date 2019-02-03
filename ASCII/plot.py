66f36e19387a842b6cb56675294ba8ec767c0ed7
Prannav Gupta <prannavgupta23@outlook.com>
2019-02-03 01:07:33 -0600

    ISSUE: Intensity calculations need to be done

--- ASCII/plot.py
+++ ASCII/plot.py
@@ -4,27 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-"""fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')"""
+fig = plt.figure()
+ax = Axes3D(fig) #fig.add_subplot(111, projection='3d')
 
 
 x, y, z, intensity, photon_count = np.loadtxt('writefile.txt', delimiter=',', unpack=True)
 
-X, Y = np.meshgrid(x, y)
-Z = np.reshape(photon_count, (-1, 2))
+"""X, Y = np.meshgrid(x, y)
+
+Z = np.reshape(photon_count, X)
 #Z = np.array(photon_count)
 #Z = Z.reshape((1, 1)
 #Z = photon_count
 #Z=np.random.rand(len(Y),len(X))
 plt.contour(X, Y, Z, colors='black')
 plt.show()
+"""
 
 
 
-"""ax.scatter(x, y, z, c='r', marker='o')
+p = ax.scatter(x, y, z, c=photon_count, marker='o') #, c='r', marker='o'
 
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 
-plt.show()"""
+fig.colorbar(p)
+
+plt.show()
