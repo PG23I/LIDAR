0da6382fba78734f5ffbb04d967a8b27beada081
Prannav Gupta <prannavgupta23@outlook.com>
2019-03-01 19:02:11 -0600

    photon_corrected

--- ASCII/helpers.py
+++ ASCII/helpers.py
@@ -1,3 +1,12 @@
 """
 This program provides various functions to correct data or to to process data
 """
+
+from math import pow
+
+#Returns the dead time corrected photon counting signal
+#INPUT: Observed count rate (ocr)
+def photon_corrected(ocr):
+    td = 260*pow(10,6) #td is the system dead time. Raymetrics uses 260 MHz
+    signal = (ocr)/(1-(td*ocr))
+    return signal
